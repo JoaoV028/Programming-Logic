1103ed3f3cadd9c72d6fd68312bcc3aef1227fc4
JoaoV028 <joao15052005@gmail.com]>
2024-01-10 09:03:04 -0300

    Aula 8 exercício

--- Aula08.py
+++ Aula08.py
@@ -1,2 +1,11 @@
 n1 = float(input('Primeira nota: '))
-n2 = float(input('Digite a segunda nota do aluno:'))
+n2 = float(input('Segunda nota do aluno:'))
+m = (n1 + n2) / 2
+print (f"Sua média é {m:.2f}!")
+if m >= 7:
+    print ('Parabéns! Você passou!')
+else:
+    if m >= 5 and m <= 7:
+     print ('Você está de recuperação!')
+    else:
+       print ('Você está de recuperação!')
